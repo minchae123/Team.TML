b3f76b31bd199d7c50545c47f50b5e6a6292c931
SungJieundev <sungjieun.dev@gmail.com>
2022-10-12 00:40:41 +0900

    2022-10-12

---
+++ Assets/01_Scripts/Jieun/Sheep And Cow/FoodMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodMove : MonoBehaviour
+{
+    public float speed = 5f;
+    public float duration = 0f;
+    private Vector3 dir;
+    
+}
