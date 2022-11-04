309beaed4abbfc698c42736c8692db8bb0ce02d2
SungJieundev <sungjieun.dev@gmail.com>
2022-11-04 21:50:57 +0900

    2022-11-04 dotween, popup

---
+++ Assets/01_Scripts/UI/PopUp.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PopUp : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject startButton;
+    private Vector3 _panelTrm = new Vector3(0, -1080, 0);
+    
+    public void PopUpPanel(){
+        CheckPanel();
+
+        startButton.SetActive(false);
+        panel.transform.DOMove(Vector3.zero, 1);
+    }
+
+    public void PopDownPanel(){
+        CheckPanel();
+
+        panel.transform.DOMove(_panelTrm, 2);
+        startButton.SetActive(true);
+
+    }
+
+    public void CheckPanel()
+    {
+        if(panel == null){
+            Debug.Log("PopUp할 패널이 비어있습니다.");
+        }
+    }
+}
