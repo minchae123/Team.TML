0b94551ed9f2b2f8670876baaebff23b322518db
minchae123 <08minchae17@gmail.com>
2022-11-15 18:43:12 +0900

    티엠피 프로

--- Assets/01_Scripts/SO/Character/Guest/Recipe.cs
+++ Assets/01_Scripts/SO/Character/Guest/Recipe.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Recipe : MonoBehaviour
 {
-    public Text showTxt;
+    //public Text showTxt;
+    public TextMeshProUGUI txt;
 
     public IngredientRecipeListSO listSO;
     public IngredientSO ingredientSO;
@@ -29,7 +31,7 @@ public class Recipe : MonoBehaviour
 
     public void TextShow()
     {
-        showTxt.text = string.Empty;
+        txt.text = string.Empty;
 
         SetIngredient(ingredientSO.meatList);
         SetIngredient(ingredientSO.vegetableList);
@@ -42,8 +44,8 @@ public class Recipe : MonoBehaviour
     {
         foreach (string ingredient in ingredients)
         {
-            showTxt.text += $"{ingredient} ";
+            txt.text += $"{ingredient} ";
         }
-        showTxt.text += "\n";
+        txt.text += "\n";
     }
 }
