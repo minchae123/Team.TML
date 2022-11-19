81f4196abf2a49e84f85199e331989dd56221c28
SungJieundev <sungjieun.dev@gmail.com>
2022-11-19 18:29:52 +0900

    밤 이벤트 완성

--- Assets/01_Scripts/UI/NightUI.cs
+++ Assets/01_Scripts/UI/NightUI.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
 
 public class NightUI : MonoBehaviour
-{
+{   
+    public Vector3 originalTransform;
     public Image fadeImage;
     public GameObject shutter;
+    public GameObject receipt;
+    public TextMeshProUGUI receiptText_Day;
+    public TextMeshProUGUI receiptText_Calculate;
+    private MainUIManager mainUIManager;
 
+    private void Awake() {
+
+        mainUIManager = GameObject.Find("Canvas").GetComponent<MainUIManager>();
+    }
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.F)){
@@ -22,12 +32,10 @@ public class NightUI : MonoBehaviour
         Sequence fadeSequence = DOTween.Sequence();
 
         fadeImage.enabled = true;
-        Debug.Log("페이드실행");
         fadeSequence.Append(fadeImage.DOFade(1f, 2f));
         
         fadeSequence.OnComplete(() => {
-            //fadeImage.DOFade(0,0.1f);
-            Debug.Log("온컴플리트");
+            
             ShutterDown();
         });
 
@@ -35,16 +43,41 @@ public class NightUI : MonoBehaviour
     }
 
     public void ShutterDown(){
+
         Debug.Log("셔터다운");
 
-        Sequence ShutterSequence = DOTween.Sequence();
+        originalTransform = shutter.transform.position;
 
+        Sequence ShutterSequence = DOTween.Sequence();
         ShutterSequence.Append(shutter.transform.DOMove(Vector3.zero, 1.5f));
         
+        ShutterSequence.OnComplete(() => {
+
+            Receipt();
+        });
 
     }
 
-    public void Calculate(){
-        
+    public void Receipt(){
+
+        receiptText_Day.text = $"{mainUIManager.dayCount}일차";
+        //receiptText_Calculate.text =  
+        receipt.transform.DOMove(Vector3.zero, 0.5f);
+
+    }
+
+    public void ReceiptButtonClick(){
+
+        Sequence moveOrigin = DOTween.Sequence();
+
+        moveOrigin.Append(shutter.transform.DOMove(originalTransform, 0.5f));
+        moveOrigin.Append(receipt.transform.DOMove(originalTransform, 0.5f));
+        moveOrigin.Append(fadeImage.DOFade(0, 1f));
+
+        moveOrigin.OnComplete(() => {
+
+            fadeImage.enabled = false;
+        });
+
     }
 }
