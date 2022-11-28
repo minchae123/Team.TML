a14c4910adaa04c8801c93798800a2c670bb1457
minchae123 <08minchae17@gmail.com>
2022-11-28 21:01:06 +0900

    ㅋㅡㄹㄹㅣㄱ ㅌㅔㄱㅅㅡㅌㅡ

--- Assets/01_Scripts/Yummy.cs
+++ Assets/01_Scripts/Yummy.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
-public class Yummy : MonoBehaviour
+public class Yummy : MonoBehaviour, IPointerClickHandler
 {
     public string ingredientName;
 
     public Sprite sp;
+
+    public TextMeshProUGUI txt;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        StartCoroutine(ShowExpain());
+    }
+
+    IEnumerator ShowExpain()
+    {
+        txt.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        txt.gameObject.SetActive(false);
+    }
 }
